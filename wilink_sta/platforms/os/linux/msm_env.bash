f726de38f99a53a22115982c266c4ad8afb1a435
jerpelea <jerpelea@gmail.com>
2011-09-01 20:40:51 +0300

    added fully working tl127x for delta and mogami boards

--- wilink_sta/platforms/os/linux/msm_env.bash
+++
@@ -1,4 +0,0 @@
-export CROSS_COMPILE=/android/cm7-test3/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/arm-eabi-
-export ARCH=arm
-export HOST_PLATFORM=msm
-export KERNEL_DIR=/android/cm7-test3/kernel
